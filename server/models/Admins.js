ce7dea26434b2d07b3f39d8a5a2d9f37e6c8f42e
Talha <talhaansari2026@gmail.com>
2025-12-02 22:59:50 +0530

    admin

--- server/models/Admins.js
+++ server/models/Admins.js
@@ -5,7 +5,7 @@ const Admins = new mongoose.Schema({
   email: { type: String, required: true, unique: true }, // Unique email for login
   password: { type: String, required: true }, // Hashed password for security
   phone: { type: String, required: true }, // Contact number
-  role: { type: String, default: "Admin" }, // Role assigned to the user
+  role: { type: String, default: "Admin" }, // Role assigned  the user
   isVerified: { type: Boolean, default: false }, // Check if admin is verified
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now }
